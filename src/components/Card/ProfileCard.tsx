50fe1603e7dc3a323550f47d385047469a1a252d
RafidRisun <rafidhassanrisun@gmail.com>
2025-12-07 14:50:56 +0600

    Cards now work like bumble

--- src/components/Card/ProfileCard.tsx
+++ src/components/Card/ProfileCard.tsx
@@ -1,17 +1,21 @@
 import {
 	iconDownArrow,
+	iconLike,
+	iconLocationLarge,
 	iconLocationSmall,
+	iconNope,
 	iconShareSmall,
 	iconSuperLike,
 	iconSwipeMessage,
 	iconVerified,
 } from '@/assets/icon';
 import tw from '@/src/lib/tailwind';
+import { Image } from 'expo-image';
 import { useRouter } from 'expo-router';
 import { useState } from 'react';
 import {
 	Dimensions,
-	Image,
+	ScrollView,
 	Share,
 	Text,
 	TouchableOpacity,
@@ -20,7 +24,6 @@ import {
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 import Animated, {
 	runOnJS,
-	useAnimatedProps,
 	useAnimatedStyle,
 	useSharedValue,
 	withSpring,
@@ -29,12 +32,12 @@ import { snapPoint } from 'react-native-redash';
 import { SvgXml } from 'react-native-svg';
 import ConfirmationModal from '../ConfirmationModal';
 
-const { width: wWidth } = Dimensions.get('window');
+const { width: wWidth, height: wHeight } = Dimensions.get('window');
 
 // const SNAP_POINTS = [-wWidth, 0, wWidth];
 
 // Wrap SvgXml with Animated
-const AnimatedSvgXml = Animated.createAnimatedComponent(SvgXml);
+//const AnimatedSvgXml = Animated.createAnimatedComponent(SvgXml);
 
 export function ProfileCard({
 	profile,
@@ -52,20 +55,9 @@ export function ProfileCard({
 	const arrowSize = useSharedValue(20);
 
 	const gesture = Gesture.Pan()
+		.activeOffsetX([-20, 20]) // Only activate after 20px horizontal movement
+		.failOffsetY([-20, 20]) // Fail if vertical movement exceeds 20px
 		.onUpdate(({ translationX, translationY }) => {
-			// Disable horizontal translation if vertical scrolling is detected
-			if (Math.abs(translationY) > Math.abs(translationX)) {
-				translateX.value = 0;
-
-				// Increase arrow size only for negative translateY (swiping down)
-				if (translationY < 0) {
-					arrowSize.value = Math.min(40, 20 + Math.abs(translationY) / 10);
-				} else {
-					arrowSize.value = 20; // Reset size for positive translateY
-				}
-				return;
-			}
-
 			translateX.value = translationX;
 
 			// Adjust opacity based on pan direction
@@ -77,7 +69,7 @@ export function ProfileCard({
 				likeOpacity.value = 0;
 			}
 		})
-		.onEnd(({ velocityX, translationY, translationX }) => {
+		.onEnd(({ velocityX }) => {
 			const dest = snapPoint(translateX.value, velocityX, [
 				-wWidth * 2,
 				0,
@@ -93,11 +85,11 @@ export function ProfileCard({
 			unlikeOpacity.value = 0;
 			arrowSize.value = withSpring(20); // Reset arrow size
 
-			// Navigate to profile on vertical swipe end only if translationY remained negative and horizontal swipe was minimal
-			if (Math.abs(translationX) < 100 && translationY < -80) {
-				//runOnJS(router.push)('/(tabs)/swipe/profile');
-				runOnJS(setBottomSheetVisible)(true);
-			}
+			// // Navigate to profile on vertical swipe end only if translationY remained negative and horizontal swipe was minimal
+			// if (Math.abs(translationX) < 100 && translationY < -80) {
+			// 	//runOnJS(router.push)('/(tabs)/swipe/profile');
+			// 	runOnJS(setBottomSheetVisible)(true);
+			// }
 		});
 
 	const animatedStyle = useAnimatedStyle(() => ({
@@ -115,14 +107,28 @@ export function ProfileCard({
 		opacity: unlikeOpacity.value,
 	}));
 
-	const arrowAnimatedProps = useAnimatedProps(() => ({
-		height: arrowSize.value,
-		width: arrowSize.value,
-	}));
+	// const arrowAnimatedProps = useAnimatedProps(() => ({
+	// 	height: arrowSize.value,
+	// 	width: arrowSize.value,
+	// }));
 
 	const [superLikeModal, setSuperLikeModal] = useState(false);
 
 	const [dmModal, setDmModal] = useState(false);
+
+	const [viewHeight, setViewHeight] = useState(0);
+
+	const handleLike = () => {
+		translateX.value = withSpring(wWidth * 2, {}, () => {
+			runOnJS(onSwipe)(profile.id);
+		});
+	};
+
+	const handleDisLike = () => {
+		translateX.value = withSpring(-wWidth * 2, {}, () => {
+			runOnJS(onSwipe)(profile.id);
+		});
+	};
 
 	return (
 		<GestureDetector gesture={gesture}>
@@ -133,8 +139,214 @@ export function ProfileCard({
 					style, // Apply the passed style
 					{ zIndex: 1 }, // Lowered zIndex to ensure modal appears above
 				]}
+				onLayout={event => {
+					const { height } = event.nativeEvent.layout;
+					setViewHeight(height); // Save the height to state
+				}}
 			>
-				<Image source={profile.image} style={tw`w-full h-full`} />
+				<ScrollView
+					style={tw`flex-1 pb-4 bg-white`}
+					showsVerticalScrollIndicator={false}
+					scrollEventThrottle={16}
+				>
+					<View style={{ width: '100%', height: viewHeight }}>
+						<Image
+							source={profile.image}
+							style={tw`w-full h-full`}
+							contentFit="cover"
+						/>
+						<View style={tw`absolute bottom-0 w-full p-4`}>
+							<View style={tw`flex-row items-center gap-2`}>
+								<Text style={tw`text-white text-3xl font-poppinsSemiBold`}>
+									{profile.name}, {profile.age}
+								</Text>
+								{profile.verified && <SvgXml xml={iconVerified} />}
+							</View>
+
+							<View style={tw`flex flex-row items-center gap-2`}>
+								<SvgXml xml={iconLocationSmall} />
+								<Text style={tw`text-white text-sm font-poppins`}>
+									{profile.distance} miles away
+								</Text>
+							</View>
+
+							<View style={tw`flex flex-row items-center justify-between mt-3`}>
+								<TouchableOpacity
+									style={tw`w-12 h-12 rounded-full bg-white items-center justify-center`}
+									onPress={() => setDmModal(true)}
+								>
+									<SvgXml xml={iconSwipeMessage} />
+								</TouchableOpacity>
+								<TouchableOpacity
+									style={tw`w-12 h-12 rounded-full bg-white items-center justify-center`}
+									onPress={() => setSuperLikeModal(true)}
+								>
+									<SvgXml xml={iconSuperLike} />
+								</TouchableOpacity>
+								<View
+									style={tw`flex w-full items-center absolute bottom-[-2] `}
+								>
+									<TouchableOpacity
+										style={tw`w-20 items-center`}
+										onPress={() => router.push('/(tabs)/swipe/profile')}
+									>
+										<SvgXml xml={iconDownArrow} width={20} height={20} />
+									</TouchableOpacity>
+								</View>
+							</View>
+						</View>
+					</View>
+					{/* header */}
+					<View
+						style={tw`flex flex-col p-4 gap-2 shadow-md bg-white rounded-lg mt-5 mx-1`}
+					>
+						<Text style={tw`text-sm font-poppinsSemiBold`}>ABOUT ME</Text>
+						<Text style={tw`text-sm font-poppins text-gray-700`}>
+							Just a creative soul looking for my next adventure. I love
+							painting on weekends and exploring new hiking trails. Big believer
+							in zodiac compatibility!
+						</Text>
+					</View>
+					<Image
+						source={require('@/assets/images/hotGuy.png')}
+						style={tw`w-full h-150 my-4`}
+					/>
+					<View
+						style={tw`flex flex-col p-4 gap-2 shadow-md bg-white rounded-lg my-5 mx-1`}
+					>
+						<Text style={tw`text-sm font-poppinsSemiBold`}>HEIGHT</Text>
+						<Text style={tw`text-sm font-poppins text-gray-700`}>
+							180cm (5&apos;9&quot;)
+						</Text>
+					</View>
+					<View
+						style={tw`flex flex-col p-4 gap-3 shadow-md bg-white rounded-lg my-5 mx-1`}
+					>
+						<Text style={tw`text-sm font-poppinsSemiBold`}>LOOKING FOR</Text>
+						<View style={tw`flex flex-row gap-2`}>
+							<View
+								style={tw`px-3 py-1 bg-gray-200 rounded-full items-center justify-center`}
+							>
+								<Text style={tw`text-sm font-poppins text-gray-700`}>
+									Something Casual
+								</Text>
+							</View>
+							<View
+								style={tw`px-3 py-1 bg-gray-200 rounded-full items-center justify-center`}
+							>
+								<Text style={tw`text-sm font-poppins text-gray-700`}>
+									Some clapping
+								</Text>
+							</View>
+						</View>
+					</View>
+					<Image
+						source={require('@/assets/images/hotGuy.png')}
+						style={tw`w-full h-150 my-4`}
+					/>
+					<View
+						style={tw`flex flex-col p-4 gap-3 shadow-md bg-white rounded-lg my-5 mx-1`}
+					>
+						<Text style={tw`text-sm font-poppinsSemiBold`}>INTERESTS</Text>
+						<View style={tw`flex flex-row gap-2`}>
+							<View
+								style={tw`px-3 py-1 bg-gray-200 rounded-full items-center justify-center`}
+							>
+								<Text style={tw`text-sm font-poppins text-gray-700`}>
+									Hiking
+								</Text>
+							</View>
+							<View
+								style={tw`px-3 py-1 bg-gray-200 rounded-full items-center justify-center`}
+							>
+								<Text style={tw`text-sm font-poppins text-gray-700`}>
+									Clapping
+								</Text>
+							</View>
+						</View>
+					</View>
+					<View
+						style={tw`flex flex-col p-4 gap-3 shadow-md bg-white rounded-lg my-5 mx-1`}
+					>
+						<Text style={tw`text-sm font-poppinsSemiBold`}>LIFESTYLE</Text>
+						<View style={tw`flex flex-row gap-2`}>
+							<View
+								style={tw`px-3 py-1 bg-gray-200 rounded-full items-center justify-center`}
+							>
+								<Text style={tw`text-sm font-poppins text-gray-700`}>
+									Smoker
+								</Text>
+							</View>
+							<View
+								style={tw`px-3 py-1 bg-gray-200 rounded-full items-center justify-center`}
+							>
+								<Text style={tw`text-sm font-poppins text-gray-700`}>
+									Drinker
+								</Text>
+							</View>
+						</View>
+					</View>
+					<View
+						style={tw`flex flex-col p-4 gap-2 shadow-md bg-white rounded-lg my-5 mx-1`}
+					>
+						<Text style={tw`text-sm font-poppinsSemiBold`}>EDUCATION</Text>
+						<Text style={tw`text-sm font-poppins text-gray-700`}>
+							Master&apos;s Degree on Bouncing Balls
+						</Text>
+					</View>
+					<View
+						style={tw`flex flex-col p-4 gap-2 shadow-md bg-white rounded-lg my-5 mx-1`}
+					>
+						<Text style={tw`text-sm font-poppinsSemiBold`}>LOCATION</Text>
+						<View style={tw`flex flex-row items-center gap-2`}>
+							<SvgXml xml={iconLocationLarge} />
+							<View style={tw`flex flex-col`}>
+								<Text style={tw`text-sm font-poppins`}>New York, NY</Text>
+								<Text style={tw`text-sm font-poppins text-gray-700`}>
+									2 miles away
+								</Text>
+							</View>
+						</View>
+					</View>
+					<View style={tw`flex flex-col gap-4 w-full py-6 mb-20`}>
+						<View style={tw`flex w-full items-center justify-center`}>
+							<TouchableOpacity
+								style={tw`w-12 h-12 rounded-full bg-white shadow-lg shadow-[#00B7DC] items-center justify-center`}
+								onPress={() => setSuperLikeModal(true)}
+							>
+								<SvgXml xml={iconSuperLike} />
+							</TouchableOpacity>
+						</View>
+						<View
+							style={tw`flex flex-row w-full px-8 items-center justify-between`}
+						>
+							<TouchableOpacity
+								style={tw`w-12 h-12 rounded-full bg-white shadow-lg shadow-[#FF0000] items-center justify-center`}
+								onPress={handleDisLike}
+							>
+								<SvgXml xml={iconNope} />
+							</TouchableOpacity>
+							<TouchableOpacity
+								style={tw`w-12 h-12 rounded-full bg-white shadow-lg shadow-[#AC48FF] items-center justify-center`}
+								onPress={handleLike}
+							>
+								<SvgXml xml={iconLike} />
+							</TouchableOpacity>
+						</View>
+						<View
+							style={tw`flex flex-col gap-5 items-center justify-center w-full absolute bottom-[-10]`}
+						>
+							<TouchableOpacity>
+								<Text style={tw`text-lg font-poppinsSemiBold`}>Block</Text>
+							</TouchableOpacity>
+							<TouchableOpacity>
+								<Text style={tw`text-lg font-poppinsSemiBold text-red-500`}>
+									Report
+								</Text>
+							</TouchableOpacity>
+						</View>
+					</View>
+				</ScrollView>
 				<TouchableOpacity
 					style={tw`absolute top-0 right-0 p-5`}
 					onPress={async () => {
@@ -166,47 +378,6 @@ export function ProfileCard({
 					]}
 				/>
 
-				<View style={tw`absolute bottom-0 w-full p-4`}>
-					<View style={tw`flex-row items-center gap-2`}>
-						<Text style={tw`text-white text-3xl font-poppinsSemiBold`}>
-							{profile.name}, {profile.age}
-						</Text>
-						{profile.verified && <SvgXml xml={iconVerified} />}
-					</View>
-
-					<View style={tw`flex flex-row items-center gap-2`}>
-						<SvgXml xml={iconLocationSmall} />
-						<Text style={tw`text-white text-sm font-poppins`}>
-							{profile.distance} miles away
-						</Text>
-					</View>
-
-					<View style={tw`flex flex-row items-center justify-between mt-3`}>
-						<TouchableOpacity
-							style={tw`w-12 h-12 rounded-full bg-white items-center justify-center`}
-							onPress={() => setDmModal(true)}
-						>
-							<SvgXml xml={iconSwipeMessage} />
-						</TouchableOpacity>
-						<TouchableOpacity
-							style={tw`w-12 h-12 rounded-full bg-white items-center justify-center`}
-							onPress={() => setSuperLikeModal(true)}
-						>
-							<SvgXml xml={iconSuperLike} />
-						</TouchableOpacity>
-						<View style={tw`flex w-full items-center absolute bottom-[-2] `}>
-							<TouchableOpacity
-								style={tw`w-20 items-center`}
-								onPress={() => router.push('/(tabs)/swipe/profile')}
-							>
-								<AnimatedSvgXml
-									xml={iconDownArrow}
-									animatedProps={arrowAnimatedProps}
-								/>
-							</TouchableOpacity>
-						</View>
-					</View>
-				</View>
 				{superLikeModal && (
 					<ConfirmationModal
 						icon="star"
